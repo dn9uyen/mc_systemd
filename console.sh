5f15bf634e25c47b8c4463e3bf3517bdd2be1e0b
dn9uyen <dannynguyen84268@gmail.com>
2023-03-17 09:04:27 -0700

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-# Replace with correct tty #
-sudo conspy 2
+# Grab TTYPath from service file
+TTY=$(grep TTYPath $"/etc/systemd/system/minecraft@${PWD##*/}.service"| awk -F= '{print $2}')
+
+# Open TTY
+conspy ${TTY:8}
